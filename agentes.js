abffa217276da9df1ec64d5492dca30edc5f0ae1
andrebelmonte19 <andrebelmonte@hotmail.com>
2021-05-21 15:59:59 -0300

    Java com Ecplipes

--- agentes.js
+++ agentes.js
@@ -8,13 +8,13 @@ function verificaUsuario(){
     usuario = JSON.parse(usuario);
     console.log("Usuario: " + usuario.nome);
 
-    document.getElementById("fotoUsuario").innerHTML = `<img src="${usuario.foto}" width="60%">`;
-    document.getElementById("bioUsuario").innerHTML = `<h3>${usuario.nome}</h3>
+    document.getElementById("fotoUsuario").innerHTML = `<center><img src="${usuario.foto}"  style="border-radius: 100%; margin-top: 10%;" width="60%" height="60%"></center>`;
+    document.getElementById("bioUsuario").innerHTML = `<h5>${usuario.nome}</h5>
                                                        <hr>
-                                                       <strong>RACF: </strong> ${usuario.racf} <br>
-                                                       <strong>EMAIL: </strong> ${usuario.email} <br>
-                                                       <strong>RAMAL: </strong> ${usuario.ramal} <br>
-                                                       <strong>DEPTO: </strong> ${usuario.depto}`;
+                                                       <strong>Racf: </strong> ${usuario.racf} <br>
+                                                       <strong>e-mail: </strong> ${usuario.email} <br>
+                                                       <strong>Ramal: </strong> ${usuario.ramal} <br>
+                                                       <strong>Depto: </strong> ${usuario.depto}`;
 
 
     fetch("http://127.0.0.1:8080/agentes")
@@ -24,10 +24,19 @@ function verificaUsuario(){
 }
 
 function preencheTabela(lista){
+
+
+    const formCurrency = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2
+    })
+
     
     var strSelect = `<br>
-                     <label for="agente">Selecione o Agente Financeiro:</label>
-                     <select id="agente" name="agente" class="form-control mb-3 mt-3" onchange="usaAgente()">`;
+                     <label for="agente"><strong>Agente Financeiro:</strong></label>
+                     <select id="agente" name="agente" class="form-control mb-3 mt-3" onchange="usaAgente()">
+                     <option value="">Selecione o agente financeiro</optin>`;
 
     var strTabela = `<table class="table table-striped table-hover">
                         <thead>
@@ -39,14 +48,15 @@ function preencheTabela(lista){
                         <tbody>`;
     
  
+
     for (i=0; i<lista.length; i++){
            var agente = lista[i];
-           strTabela = strTabela + `<tr>
-                                        <td> ${agente.nome} </td>
-                                        <td> ${agente.volume} </td>
+           strTabela = strTabela + `<tr onclick="usaAgente(${agente.id})">
+                                        <td> ${agente.nome.toUpperCase()} </td>
+                                        <td> ${formCurrency.format(agente.volume)} </td>
                                     </td>`
             
-            strSelect = strSelect + `<option value="${agente.id}"> ${agente.nome} </optin>`;
+            strSelect = strSelect + `<option value="${agente.id}"> ${agente.nome.toUpperCase()} </optin>`;
     }
 
     strTabela = strTabela + `</tbody>
@@ -60,13 +70,12 @@ function preencheTabela(lista){
 }
 
 
-function usaAgente(){
-    var idAgente = document.getElementById("agente").value;
+function usaAgente(idAgente){
+    
+    if(!idAgente){
+        var idAgente = document.getElementById("agente").value;
+    }
     window.location = "dashboard.html?id=" + idAgente;
-
-
-    //console.log(idAgente);
-
 }
 
 function logout(){
--- dashboard.js
+++ dashboard.js
@@ -15,15 +15,65 @@ function geraRelatorio(){
     fetch("http://localhost:8080/transacoestotais/"+id)
         .then(res=>res.json())
         .then(totais => montaDashBoard(totais));
+      
 }
 
 function montaDashBoard(totais){
+
+    const formCurrency = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2
+    })
     
-    document.getElementById("nomeAgente").innerHTML = "<h5>"+totais.nomeAgente+"</h5>";
-    document.getElementById("volumeAgente").innerHTML = totais.volume;
+    document.getElementById("nomeAgente").innerHTML = "<h5>"+totais.nomeAgente.toUpperCase()+"</h5>";
+    document.getElementById("volumeAgente").innerHTML = formCurrency.format(totais.volume);
     document.getElementById("sucesso").innerHTML = totais.totalSucesso;
     document.getElementById("falha").innerHTML = totais.totalFalha;
     document.getElementById("fraude").innerHTML = totais.totalFraude;
+   
+
+    var ctx = document.getElementById('meuGrafico');
+    var meuGrafico = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Sucesso', 'Falha', 'Fraude'],
+            datasets: [{
+                data: [totais.totalSucesso,totais.totalFalha,totais.totalFraude],
+                label: 'Total de Transações',
+                backgroundColor: [
+                    'rgb(24, 130, 81)',
+                    'rgb(247, 186, 6)',
+                    'rgb(213, 51, 66)']
+            }],
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: false,
+                },
+
+                title: {
+                    display: true,
+                    text: 'Dashbord Cartões',
+                    family: 'Unlock',
+                    padding: {
+                        top: 10,
+                        bottom: 30
+                    },
+                    font: {
+                        size: 20
+                    },
+                                       
+                }              
+            }
+        }
+ 
+
+
+    });
+
+
 }
 
 
@@ -35,5 +85,5 @@ function logout(){
 }
 
 function goBack() {
-    window.history.back();
+    window.location = "agentes.html";
 }
